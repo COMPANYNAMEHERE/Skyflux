8c7d22f602afbe1ff5352eacbe84e9d5ba540a8a
COMPANYNAMEHERE <scientiafilms@gmail.com>
2024-10-02 12:41:13 +0200

    Initial commit

---
+++ src/main/java/net/josso/skyflux/SkyFluxClient.java
@@ -0,0 +1,10 @@
+package net.josso.skyflux;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkyFluxClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/josso/skyflux/SkyFluxDataGenerator.java
@@ -0,0 +1,11 @@
+package net.josso.skyflux;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SkyFluxDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
